a276646b216f1bcacf64afcf63764a9aa69a1bad
Omar Rayes <omarrayes1998@gmail.com>
2025-06-17 10:04:33 +0300

    fix:login:

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,10 +3,14 @@ import * as authController from "@/controllers/authController";
 import upload from "@/middleware/registerPhotoUpload";
 
 const authRouter = express.Router();
-authRouter.post("/register",upload.single('profile_image'), authController.register);
-authRouter.post("/login", authController.login);
+authRouter.post(
+  "/register",
+  upload.single("profile_image"),
+  authController.register
+);
+
+authRouter.post("/login", upload.none(), authController.login);
 
 authRouter.post("/logout", authController.logout);
-
 
 export default authRouter;
